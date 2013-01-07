1a33b19f26ce133c446668b7704f519e44badc0d
Jim Blanchard <jimmyblanchard@gmail.com>
2013-01-07 18:32:02 -0500

    [Edit] Modified hardware manager folder structure.

---
+++ Hardware_Manager/HardwareManager.py
@@ -0,0 +1,14 @@
+# Mercury2 Hardware Manager
+# Version: 0.1.0
+# Website: http://exploration.engin.umich.edu/blog/
+# Created by the Michigan Exploration Laboratory
+#
+# Enables standardized ground station control over a network for a variety of hardware devices. Interacts with the User
+# Interface component of the Mercury2 system to allow for schedule management and a control interface. This bootstrap 
+# file is responsible for initializing the application.
+
+# Import the required libraries
+
+
+# Run the initialization script
+
