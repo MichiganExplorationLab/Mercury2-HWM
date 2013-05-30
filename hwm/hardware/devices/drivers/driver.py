6e3a5d7fa7a99a4a360f9aa42d155055cdfdf138
Jimmy Blanchard <jimmyblanchard@gmail.com>
2013-05-29 21:21:56 -0400

    [Edit] Converted the thread locks to use actual locks instead of booleans.

--- hwm/hardware/devices/drivers/driver.py
+++ hwm/hardware/devices/drivers/driver.py
@@ -3,7 +3,7 @@ Defines the base driver class that all other device drivers must extend.
 """
 
 # Import required modules
-import logging
+import logging, threading
 
 class Driver:
   """ Provides the base driver interface.
@@ -23,26 +23,23 @@ class Driver:
     
     # Set driver attributes
     self.settings = device_configuration
-    self.in_use = False
+    self.access_lock = threading.Lock()
   
   def reserve_device(self):
     """ Reserves the device for a reservation.
     
     This method checks if the device is currently being used and, if it isn't, updates the usage flag (in_use).
     
-    @note Typically, only sessions should need to reserve devices. This prevents two sessions from accidentally allowing
-          access to the same device at the same time while still allowing access to the driver for other things (like 
-          admin non-session commands), which wouldn't be the case if normal mutex's were used.
+    @note Typically, only sessions should need to reserve devices for access. This prevents two sessions from 
+          accidentally allowing access to the same device at the same time while still allowing access to the driver for
+          other things (like admin non-session commands).
     
     @throw Throws DeviceInUse if the device has already been reserved by another session.
     """
     
     # Check if the device is being used
-    if self.in_use:
+    if not self.access_lock.acquire(False):
       raise DeviceInUse("The requested device is currently being used and can't be reserved.")
-    
-    # Lock the device
-    self.in_use = True
   
   def free_device(self):
     """ Frees the device usage lock.
@@ -51,7 +48,10 @@ class Driver:
     """
     
     # Free the device
-    self.in_use = False
+    try:
+      self.access_lock.release()
+    except ThreadError:
+      pass
   
 # Define custom driver exceptions
 class DeviceInUse(Exception):
